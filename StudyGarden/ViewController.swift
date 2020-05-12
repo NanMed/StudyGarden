ff33dbd1b2308cd05a419e9c343f15cf0e9ec050
NanMed <46913776+NanMed@users.noreply.github.com>
2020-05-11 21:34:54 -0500

    ImgSelector

--- StudyGarden/ViewController.swift
+++ StudyGarden/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var buttonCamara: UIButton!
+    @IBOutlet weak var fotoVista: UIImageView!
+    private let miPicker = UIImagePickerController()
+    
     override func viewDidLoad() {
+        print("Dentro")
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //if !UIImagePickerController.isSourceTypeAvailable(.camera){
+            //buttonCamara.isHidden = true
+        //}
+        miPicker.delegate = self
     }
 
-
+    @IBAction func camara() {
+        miPicker.sourceType = UIImagePickerController.SourceType.camera
+        present(miPicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func guardarImg() {
+        UIImageWriteToSavedPhotosAlbum(fotoVista.image!, nil, nil, nil)
+    }
+    @IBAction func album() {
+        print("Dentro")
+        miPicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        present(miPicker, animated: true, completion: nil)
+    }
+    
+    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        fotoVista.image = info[UIImagePickerController.InfoKey.originalImage.rawValue] as? UIImage
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
 
