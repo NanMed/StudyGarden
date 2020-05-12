ad539fd3dfa156cf8db973cf01ce9d05ea7db128
Natalia <A01338719@itesm.mx>
2020-05-12 13:11:49 -0400

    avances de json

---
+++ StudyGarden/TableViewMateriasViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TableViewMateriasViewController.swift
+//  StudyGarden
+//
+//  Created by user168001 on 5/12/20.
+//  Copyright © 2020 ProyectoFinal. All rights reserved.
+//
+
+import UIKit
+
+class TableViewMateriasViewController: UIViewController {
+
+    @IBOutlet weak var TablbeViewMaterias: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
