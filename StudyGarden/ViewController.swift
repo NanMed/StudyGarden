2e5cc6218a93bf7a69929295ab05fe43d674f699
NanMed <46913776+NanMed@users.noreply.github.com>
2020-05-12 18:27:06 -0500

    Revert "Album pt2"
    This reverts commit a074e59ccf55fdcc5d16bb2c438a7886db19b33e.

--- StudyGarden/ViewController.swift
+++ StudyGarden/ViewController.swift
@@ -12,29 +12,27 @@ import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var fotoVista: UIImageView!
     private let miPicker = UIImagePickerController()
-    
-    
-    @IBOutlet weak var viewFoto: UIImageView!
-    @IBOutlet weak var botonCamara: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //if !UIImagePickerController.isSourceTypeAvailable(.camera){
-            //botonCamara.isHidden = true
+            //buttonCamara.isHidden = true
         //}
         miPicker.delegate = self
     }
-    
-    @IBAction func guardarImg(_ sender: Any) {
-        UIImageWriteToSavedPhotosAlbum(viewFoto.image!, nil, nil, nil)
-    }
-    
-    //@IBAction func camara(_ sender: Any) {
+
+    //@IBAction func camara() {
         //miPicker.sourceType = UIImagePickerController.SourceType.camera
         //present(miPicker, animated: true, completion: nil)
     //}
+    
+    //@IBAction func guardarImg() {
+        //UIImageWriteToSavedPhotosAlbum(fotoVista.image!, nil, nil, nil)
+    //}
+    
     
     @IBAction func album(_ sender: Any) {
         print("Dentro")
@@ -43,7 +41,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        viewFoto.image = (info[UIImagePickerController.InfoKey.originalImage.rawValue] as? UIImage)!
+        print("Dentro de nuevo")
+        fotoVista.image = info[UIImagePickerController.InfoKey.originalImage.rawValue] as? UIImage
         picker.dismiss(animated: true, completion: nil)
     }
     
