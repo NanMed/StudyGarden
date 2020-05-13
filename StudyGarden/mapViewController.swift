881f92315e90569ae93a2bf10b4ceab4cfe7cf01
JimeBB <jimeberbau@outlook.es>
2020-05-13 16:49:00 -0500

    Mapa con árboles

--- StudyGarden/mapViewController.swift
+++ StudyGarden/mapViewController.swift
@@ -6,50 +6,61 @@
 //  Copyright © 2020 ProyectoFinal. All rights reserved.
 //
 
-import MapKit
-import CoreLocation
 import UIKit
+import MapKit
 
-class mapViewController: UIViewController,CLLocationManagerDelegate {
 
-    @IBOutlet weak var mapa: MKMapView!
+class mapViewController: UIViewController, MKMapViewDelegate {
+    let treeAnnotations = TreeAnnotations()
+    let initialCoordinate = CLLocationCoordinate2DMake(19.3576047,-99.2700138)
+  
+    @IBOutlet weak var mapView: MKMapView!
     
-    private let locationManager = CLLocationManager()
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        var annotationView = MKMarkerAnnotationView()
+        guard let annotation = annotation as? TreeAnnotation else {return nil}
+        var identifier = ""
+        var color = UIColor.red
+        switch annotation.type{
+        case .progra:
+            identifier = "Programación Avanzada"
+            color = .green
+        case .calidad:
+            identifier = "Calidad y Pruebas"
+            color = .green
+        case .amss:
+            identifier = "AMSS"
+            color = .green
+        }
+        if let dequedView = mapView.dequeueReusableAnnotationView(
+            withIdentifier: identifier)
+            as? MKMarkerAnnotationView {
+            annotationView = dequedView
+        } else{
+            annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        }
+        annotationView.markerTintColor = color
+        annotationView.glyphImage = UIImage(named: "tree")
+
+        return annotationView
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-    locationManager.requestWhenInUseAuthorization()
-        mapa.mapType=MKMapType.standard
-        let cl=CLLocationCoordinate2DMake(19.3574332,-99.2705678)
-        //mapa.region=MKCoordinateRegion(center: cl, latitudinalMeters: 2000, longitudinalMeters: 2000)
-        let delta=CLLocationDegrees(0.01)
-        let span=MKCoordinateSpan(latitudeDelta: delta, longitudeDelta: delta)
-        mapa.region=MKCoordinateRegion(center: cl, span: span)
-        let rest = MKPointAnnotation()
-        rest.coordinate = cl
-        rest.title = "Árbol"
-        rest.subtitle = "Programación Avanzada"
-        mapa.addAnnotation(rest)
-        mapa.showsCompass=true
-        mapa.showsScale=true
-        mapa.showsTraffic=true
-        mapa.isZoomEnabled=true
-        
-        
+        mapView.delegate = self
+        //set intial region
+        let initialregion = MKCoordinateRegion(center: initialCoordinate, span: MKCoordinateSpanMake(0.25, 0.25))
+        mapView.setRegion(initialregion, animated: true)
+        // add the annotations
+        mapView.addAnnotations(treeAnnotations.restaurants)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
-    func locationManager(_ manager: CLLocationManager,
-                         didChangeAuthorization status: CLAuthorizationStatus) {
-        if status == .authorizedWhenInUse{
-            locationManager.startUpdatingLocation()
-            mapa.showsUserLocation = true
-        } else {
-            locationManager.stopUpdatingLocation()
-            mapa.showsUserLocation = false
-        }
-    }
 
 }
+
---
+++ StudyGarden/treeMarkersController.swift
@@ -0,0 +1,44 @@
+//
+//  treeMarkersController.swift
+//  StudyGarden
+//
+//  Created by Jimena Bermudez Bautista on 13/05/20.
+//  Copyright © 2020 ProyectoFinal. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+enum TreeType {
+    case progra
+    case calidad
+    case amss
+}
+
+class TreeAnnotation:NSObject,MKAnnotation{
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    var type: TreeType
+    init(_ latitude:CLLocationDegrees,_ longitude:CLLocationDegrees,title:String,subtitle:String,type:TreeType){
+        self.coordinate = CLLocationCoordinate2DMake(latitude, longitude)
+        self.title = title
+        self.subtitle = subtitle
+        self.type = type
+    }
+}
+
+
+class TreeAnnotations: NSObject {
+    var restaurants:[TreeAnnotation]
+    
+    override init(){
+       //build an array of pizza loactions literally
+        restaurants = [TreeAnnotation(19.3573267,-99.2699144, title: "Árbol Uno", subtitle:"Programación Avanzada", type: .progra)]
+        restaurants += [TreeAnnotation(19.3571951,-99.2698775, title: "Árbol Dos", subtitle:"Calidad y Pruebas", type: .calidad)]
+        restaurants += [TreeAnnotation(19.3571528,-99.2697883, title: "Árbol tres", subtitle:"AMSS", type: .amss)]
+       
+        
+    }
+}
+
