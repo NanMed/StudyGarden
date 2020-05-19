15dc4eafe8513da070576c621d66d5d4f71b4c6e
NanMed <46913776+NanMed@users.noreply.github.com>
2020-05-18 22:09:53 -0500

    Corrección controler Explorar

---
+++ StudyGarden/ExploraObjetosViewController.swift
@@ -0,0 +1,91 @@
+//
+//  ExploraObjetosViewController.swift
+//  StudyGarden
+//
+//  Created by Nancy Medina on 18/05/20.
+//  Copyright © 2020 ProyectoFinal. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ExploraObjetosViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+
+    @IBOutlet weak var fotoVista: UIImageView!
+    @IBOutlet weak var botonCamara: UIButton!
+    @IBOutlet weak var resultadoML: UILabel!
+    
+    private let miPicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        if !UIImagePickerController.isSourceTypeAvailable(.camera){
+            //Desactiva el boton de la camara si no hay camara
+            botonCamara.isEnabled = false
+            botonCamara.alpha = 0.5
+        }
+        miPicker.delegate = self
+    }
+    
+    @IBAction func camara(_ sender: Any) {
+        miPicker.sourceType = UIImagePickerController.SourceType.camera
+        present(miPicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func guardarImg(_ sender: Any) {
+        UIImageWriteToSavedPhotosAlbum(fotoVista.image!, nil, nil, nil)
+    }
+    
+    @IBAction func album(_ sender: Any) {
+        miPicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        miPicker.allowsEditing = true
+        present(miPicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        fotoVista.image = info[UIImagePickerControllerEditedImage] as? UIImage
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func ejecutarML() {
+        //print("aqui")
+        //instanciar el modelo de la red neuronal
+        let modelFile = ProyectoSG()
+        let model = try! VNCoreMLModel(for: modelFile.model)
+        //Convertir la imagen obtenida a CIImage
+        let imagenCI = CIImage(image: fotoVista.image!)
+        //Crear un controlador para el manejo de la imagen, este es un requerimiento para ejecutar la solicitud del modelo
+        let handler = VNImageRequestHandler(ciImage: imagenCI!)
+        //Crear una solicitud al modelo para el análisis de la imagen
+        let request = VNCoreMLRequest(model: model, completionHandler: resultadosModelo)
+        //print("aqui ya no")
+        try! handler.perform([request])
+    }
+    
+    func resultadosModelo(request: VNRequest, error: Error?)
+    {
+        //print("aqui si")
+        guard let results = request.results as? [VNClassificationObservation] else { fatalError("No hubo respuesta del modelo ML")}
+        var bestPrediction = ""
+        var bestConfidence: VNConfidence = 0
+        //recorrer todas las respuestas en búsqueda del mejor resultado
+        for classification in results{
+            print(classification.confidence)
+            print(classification.identifier)
+            if (classification.confidence > bestConfidence){
+                bestConfidence = classification.confidence
+                bestPrediction = classification.identifier
+            }
+        }
+        let resultado = bestPrediction+" "+String(bestConfidence)
+        print(resultado)
+        resultadoML.text = resultado
+    }
+}
