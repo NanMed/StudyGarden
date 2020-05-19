60cfeec584e4619b32d56d1e24c9754a645c3484
NanMed <46913776+NanMed@users.noreply.github.com>
2020-05-18 20:47:35 -0500

    PruebaML

--- StudyGarden/ViewController.swift
+++ StudyGarden/ViewController.swift
@@ -7,19 +7,25 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var fotoVista: UIImageView!
-    @IBOutlet weak var buttonCamara: UIButton!
+    @IBOutlet weak var botonCamara: UIButton!
+    //@IBOutlet weak var resultadoML: UILabel!
+    
     private let miPicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        //if !UIImagePickerController.isSourceTypeAvailable(.camera){
-            //buttonCamara.isHidden = true
-        //}
+        botonCamara.isHidden = false
+        if !UIImagePickerController.isSourceTypeAvailable(.camera){
+            print("Entra")
+            botonCamara.isHidden = true
+        }
         miPicker.delegate = self
     }
     
@@ -46,5 +52,37 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
     }
+    
+    /*@IBAction func ejecutarML() {
+        //instanciar el modelo de la red neuronal
+        let modelFile = TestStudy()
+        let model = try! VNCoreMLModel(for: modelFile.model)
+        //Convertir la imagen obtenida a CIImage
+        let imagenCI = CIImage(image: fotoVista.image!)
+        //Crear un controlador para el manejo de la imagen, este es un requerimiento para ejecutar la solicitud del modelo
+        let handler = VNImageRequestHandler(ciImage: imagenCI!)
+        //Crear una solicitud al modelo para el análisis de la imagen
+        let request = VNCoreMLRequest(model: model, completionHandler: resultadosModelo)
+        try! handler.perform([request])
+    }
+    
+    func resultadosModelo(request: VNRequest, error: Error?)
+    {
+        guard let results = request.results as? [VNClassificationObservation] else { fatalError("No hubo respuesta del modelo ML")}
+        var bestPrediction = ""
+        var bestConfidence: VNConfidence = 0
+        //recorrer todas las respuestas en búsqueda del mejor resultado
+        for classification in results{
+            print(classification.confidence)
+            print(classification.identifier)
+            if (classification.confidence > bestConfidence){
+                bestConfidence = classification.confidence
+                bestPrediction = classification.identifier
+            }
+        }
+        let resultado = bestPrediction+" "+String(bestConfidence)
+        print(resultado)
+        resultadoML.text = resultado
+    }*/
 }
 
